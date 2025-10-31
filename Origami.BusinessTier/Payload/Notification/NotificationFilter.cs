29ab8558d444fd65874901531e2c041c20af6c07
ntrongphuc <ntrongphuc.1302@gmail.com>
2025-10-31 17:03:05 +0700

    Add missing file of previous commit

---
+++ Origami.BusinessTier/Payload/Notification/NotificationFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Origami.BusinessTier.Payload.Notification
+{
+    public class NotificationFilter
+    {
+        public bool? IsRead { get; set; } = null!;
+        //public DateTime? CreatedAfter { get; set; } = null!;
+        //public DateTime? CreatedBefore { get; set; } = null!;
+        public DateTime? CreatedAt { get; set; } = null!;
+    }
+}
---
+++ Origami.BusinessTier/Payload/Notification/NotificationInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Origami.BusinessTier.Payload.Notification
+{
+    public class NotificationInfo
+    {
+        public int? UserId { get; set; }
+
+        public string? Content { get; set; }
+
+        public bool? IsRead { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
