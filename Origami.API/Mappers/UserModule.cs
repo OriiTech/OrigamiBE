97ad8d7ba419707a97bc67b62f3ecadf7b1fb278
MinhNg666 <MinhNg666@users.noreply.github.com>
2025-11-03 20:58:00 +0700

    fix bug in UserModule

--- Origami.API/Mappers/UserModule.cs
+++ Origami.API/Mappers/UserModule.cs
@@ -10,7 +10,7 @@ namespace Origami.API.Mappers
         public UserModule()
         {
             CreateMap<User, GetUserResponse>()
-                 .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.Role.RoleName));
+                 .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.Role.RoleId));
             CreateMap<RegisterRequest, User>()
           .ForMember(dest => dest.Password, opt => opt.Ignore())
           .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow));
