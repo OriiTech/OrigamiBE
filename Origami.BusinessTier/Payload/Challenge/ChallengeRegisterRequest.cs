5a1a4a1d52f395e329e5e1555ef702dc5963e210
MinhNg666 <MinhNg666@users.noreply.github.com>
2025-11-18 18:38:43 +0700

    pull and merge from master

---
+++ Origami.BusinessTier/Payload/Challenge/ChallengeRegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Origami.BusinessTier.Payload.Challenge
+{
+    public class ChallengeRegisterRequest
+    {
+        public int ChallengeId { get; set; }
+        public int? TeamId { get; set; }
+        public string? TeamName { get; set; }
+    }
+}
