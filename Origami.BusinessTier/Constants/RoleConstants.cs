a406973225baed6645ec4a62064171d24e6b8d96
MinhNg666 <MinhNg666@users.noreply.github.com>
2026-01-11 00:18:54 +0700

    Add RoleConstants.cs for role-based authorization

---
+++ Origami.BusinessTier/Constants/RoleConstants.cs
@@ -0,0 +1,9 @@
+namespace Origami.BusinessTier.Constants
+{
+    public static class RoleConstants
+    {
+        public const string User = "1";
+        public const string Sensei = "2";
+        public const string Staff = "3";
+    }
+}
